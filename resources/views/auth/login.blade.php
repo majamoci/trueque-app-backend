116b332eb709b1ea246a160ffc752cf1f23e193b
thegreatyamori <jersonmorocho16@gmail.com>
2020-06-25 00:42:25 -0500

    HU-0009-T4
    - Modificar Escenario "Login"
    - Modificar Formulario "login" (username + password + submit button)
    - Código backend para loguear usuario,

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,76 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+<div class="mdl-layout mdl-js-layout color--gray is-small-screen login">
+  <main class="mdl-layout__content">
+    <div class="mdl-card mdl-card__login mdl-shadow--2dp">
+      <div class="mdl-card__supporting-text color--dark-gray">
+        <div class="mdl-grid">
+          <form method="POST" action="{{ route('login') }}">
+            @csrf
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
+            <div class="mdl-cell mdl-cell--12-col mdl-cell--4-col-phone">
+              <span class="mdl-card__title-text text-color--smooth-gray">
+                {{ __('Sistema de Intercambio Digital') }}
+              </span>
             </div>
+            <div class="mdl-cell mdl-cell--12-col mdl-cell--4-col-phone">
+              <span class="login-name text-color--white">{{ __('Ingreso') }}</span>
+              <span class="login-secondary-text text-color--smoke">
+                {{ __('Complete los campos') }} *
+              </span>
+            </div>
+            <div class="mdl-cell mdl-cell--12-col mdl-cell--4-col-phone">
+              <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label full-size">
+                <input type="text" name="email" id="email" value="{{ old('email') }}" required
+                  class="mdl-textfield__input @error('email') is-invalid @enderror">
+                <label class="mdl-textfield__label" for="email">
+                  {{ __('Correo Electrónico') }} *
+                </label>
+
+                @error('email')
+                <span class="invalid-feedback" role="alert">
+                  <strong>{{ $message }}</strong>
+                </span>
+                @enderror
+              </div>
+
+              <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label full-size">
+                <input id="password" type="password" name="password" required
+                  class="mdl-textfield__input @error('password') is-invalid @enderror">
+                <label class="mdl-textfield__label" for="password">{{ __('Contraseña') }} *</label>
+
+                @error('password')
+                <span class="invalid-feedback" role="alert">
+                  <strong>{{ $message }}</strong>
+                </span>
+                @enderror
+              </div>
+
+              <div class="col-md-6 offset-md-4">
+                <label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect" for="remember">
+                  <input type="checkbox" id="remember" type="checkbox" name="remember" class="mdl-checkbox__input"
+                    {{ old('remember') ? 'checked' : '' }}>
+                  <span class="mdl-checkbox__label">{{ __('Recordarme') }}</span>
+                </label>
+              </div>
+            </div>
+            <div class="mdl-cell mdl-cell--12-col mdl-cell--4-col-phone submit-cell">
+              @if (Route::has('password.request'))
+              <a class="login-link" href="{{ route('password.request') }}">
+                {{ __('¿Olvidó la contraseña?') }}
+              </a>
+              @endif
+              <div class="mdl-layout-spacer"></div>
+              <button type="submit"
+                class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect color--light-blue">
+                {{ __('Ingresar') }}
+              </button>
+            </div>
+          </form>
         </div>
+      </div>
     </div>
+  </main>
 </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,58 +11,36 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
-    <script src="{{ asset('js/core/bootstrap-material-design.min.js') }}" defer></script>
-    <script src="{{ asset('js/plugins/perfect-scrollbar.jquery.min.js') }}" defer></script>
-    <!-- Plugin for the momentJs  -->
-    <script src="{{ asset('js/plugins/moment.min.js') }}" defer></script>
-    <!--  Plugin for Sweet Alert -->
-    <script src="{{ asset('js/plugins/sweetalert2.js') }}" defer></script>
-    <!-- Forms Validations Plugin -->
-    <script src="{{ asset('js/plugins/jquery.validate.min.js') }}" defer></script>
-    <!-- Plugin for the Wizard, full documentation here: https://github.com/VinceG/twitter-bootstrap-wizard -->
-    <script src="{{ asset('js/plugins/jquery.bootstrap-wizard.js') }}" defer></script>
-    <!--	Plugin for Select, full documentation here: http://silviomoreto.github.io/bootstrap-select -->
-    <script src="{{ asset('js/plugins/bootstrap-selectpicker.js') }}" defer></script>
-    <!--  Plugin for the DateTimePicker, full documentation here: https://eonasdan.github.io/bootstrap-datetimepicker/ -->
-    <script src="{{ asset('js/plugins/bootstrap-datetimepicker.min.js') }}" defer></script>
-    <!--  DataTables.net Plugin, full documentation here: https://datatables.net/  -->
-    <script src="{{ asset('js/plugins/jquery.dataTables.min.js') }}" defer></script>
-    <!--	Plugin for Tags, full documentation here: https://github.com/bootstrap-tagsinput/bootstrap-tagsinputs  -->
-    <script src="{{ asset('js/plugins/bootstrap-tagsinput.js') }}" defer></script>
-    <!-- Plugin for Fileupload, full documentation here: http://www.jasny.net/bootstrap/javascript/#fileinput -->
-    <script src="{{ asset('js/plugins/jasny-bootstrap.min.js') }}" defer></script>
-    <!--  Full Calendar Plugin, full documentation here: https://github.com/fullcalendar/fullcalendar    -->
-    <script src="{{ asset('js/plugins/fullcalendar.min.js') }}" defer></script>
-    <!-- Vector Map plugin, full documentation here: http://jvectormap.com/documentation/ -->
-    <script src="{{ asset('js/plugins/jquery-jvectormap.js') }}" defer></script>
-    <!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
-    <script src="{{ asset('js/plugins/nouislider.min.js') }}" defer></script>
-    <!-- Include a polyfill for ES6 Promises (optional) for IE11, UC Browser and Android browser support SweetAlert -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/core-js/2.4.1/core.js"></script>
-    <!-- Library for adding dinamically elements -->
-    <script src="{{ asset('js/plugins/arrive.min.js') }}" defer></script>
-    <!--  Google Maps Plugin    -->
-    <!-- <script src="{{ asset('aps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE') }}" defer></script> -->
-    <!-- Chartist JS -->
-    <script src="{{ asset('js/plugins/chartist.min.js') }}" defer></script>
-    <!--  Notifications Plugin    -->
-    <script src="{{ asset('js/plugins/bootstrap-notify.js') }}" defer></script>
-    <!-- Control Center for Material Dashboard: parallax effects, scripts for the example pages etc -->
-    <script src="{{ asset('js/material-dashboard.js') }}" type="text/javascript" defer></script>
-    <script src="{{ asset('js/bundle.js') }}" defer></script>
+    <script src="{{ asset('js/d3.min.js') }}"></script>
+    <script src="{{ asset('js/getmdl-select.min.js') }}"></script>
+    <script src="{{ asset('js/material.min.js') }}"></script>
+    <script src="{{ asset('js/nv.d3.min.js') }}"></script>
+    <script src="{{ asset('js/layout/layout.min.js') }}"></script>
+    <script src="{{ asset('js/scroll/scroll.min.js') }}"></script>
+    <script src="{{ asset('js/widgets/charts/discreteBarChart.min.js') }}"></script>
+    <script src="{{ asset('js/widgets/charts/linePlusBarChart.min.js') }}"></script>
+    <script src="{{ asset('js/widgets/charts/stackedBarChart.min.js') }}"></script>
+    <script src="{{ asset('js/widgets/employer-form/employer-form.min.js') }}"></script>
+    <script src="{{ asset('js/widgets/line-chart/line-charts-nvd3.min.js') }}"></script>
+    <script src="{{ asset('js/widgets/map/maps.min.js') }}"></script>
+    <script src="{{ asset('js/widgets/pie-chart/pie-charts-nvd3.min.js') }}"></script>
+    <script src="{{ asset('js/widgets/table/table.min.js') }}"></script>
+    <script src="{{ asset('js/widgets/todo/todo.min.js') }}"></script>
 
     <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+    <link href='https://fonts.googleapis.com/css?family=Roboto:400,500,300,100,700,900' rel='stylesheet'
+          type='text/css'>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/lib/getmdl-select.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/lib/nv.d3.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/application.min.css') }}">
 </head>
-<body  class="">
+<body>
+    <!-- id="app" inyecta vue components -->
     <div id="app">
-        <main class="py-4">
-            @yield('content')
-        </main>
+        @yield('content')
     </div>
 </body>
 </html>
