02764eeb4341d0ed67c38c18f2dcf4f62a0d0d74
thegreatyamori <jersonmorocho16@gmail.com>
2020-08-02 18:03:35 -0500

    Cambio de timezone e idioma

---
+++ resources/lang/es/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Su contraseña ha sido reseteada!',
+    'sent' => 'Hemos enviado un link para resetear la contraseña a su e-mail!',
+    'throttled' => 'Por favor espere antes de intentar.',
+    'token' => 'El token de reseteo de la contraseña es inválido.',
+    'user' => "No encontramos ningun usuario con esa dirección de e-mail.",
+
+];
